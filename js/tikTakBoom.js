ee41ed66cf09bf123aea27ee274a7e648601c5cd
Anna Yakovleva <89506212679@mail.ru>
2020-09-22 21:09:40 +0300

    revert changes for gameControls

--- js/tikTakBoom.js
+++ js/tikTakBoom.js
@@ -173,8 +173,8 @@ tikTakBoom = {
         this.textFieldAnswer3.style.display = "none";
         this.textFieldAnswer4.style.display = "none";
         this.textFieldAnswer5.style.display = "none";
-        //this.playerNum.style.display = "block";
-        //this.startGameDiv.style.display = "block";
+        this.playerNum.style.display = "block";
+        this.startGameDiv.style.display = "block";
     },
 
     showGameControls() {
@@ -184,8 +184,8 @@ tikTakBoom = {
         this.textFieldAnswer3.style.display = "block";
         this.textFieldAnswer4.style.display = "block";
         this.textFieldAnswer5.style.display = "block";
-        this.playerNum.style.display = "none";  
-        this.startGameDiv.style.display = "none";   
+        //this.playerNum.style.display = "none";  
+        //this.startGameDiv.style.display = "none";   
     },
 
     run() {
